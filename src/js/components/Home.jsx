9d659898d4722a8e4f877e3b79f7b87d6df51080
monkeyjp <43521145+monkeyjp@users.noreply.github.com>
2026-06-10 17:55:03 +0000

    navbar component

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,34 +2,38 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./Navbar";
 
 //create your first component
 const Home = () => {
-  return (
-    <div className="text-center">
-      <div id="carouselExample" class="carousel slide">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="..." class="d-block w-100" alt="..." />
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..." />
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<Navbar />
+			<div className="text-center">
+				<div id="carouselExample" class="carousel slide">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img src="..." class="d-block w-100" alt="..." />
+						</div>
+						<div class="carousel-item">
+							<img src="..." class="d-block w-100" alt="..." />
+						</div>
+						<div class="carousel-item">
+							<img src="..." class="d-block w-100" alt="..." />
+						</div>
+					</div>
+					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Previous</span>
+					</button>
+					<button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Next</span>
+					</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Home;
